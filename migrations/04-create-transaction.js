3d023ab9960a208e2e4efecb86e354b0b57bf506
cowdingus <zaki.nadhif45@gmail.com>
2022-05-19 09:04:48 +0700

    Fix migration ordering

--- migrations/04-create-transaction.js
+++ migrations/04-create-transaction.js
@@ -12,7 +12,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: "Member",
+          model: "Members",
           key: "id"
         }
       },
@@ -44,7 +44,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: "User",
+          model: "Users",
           key: "id"
         }
       },
